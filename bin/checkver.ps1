73ede4a783a0501b4ef0f99b519a651a50c44b3b
Marcos Vinícius Rodrigues Pires de Lima <mvrpl.me@gmail.com>
2024-05-05 14:38:55 -0300

    remove checkver

--- bin/checkver.ps1
+++
@@ -1,4 +0,0 @@
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
-$checkver = "$env:SCOOP_HOME/bin/checkver.ps1"
-$dir = "~\scoop\buckets\mvrpl"
-Invoke-Expression -command "& '$checkver' -dir '$dir' $($args | ForEach-Object { "$_ " })"
