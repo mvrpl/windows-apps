3ca72c2282c6904aa8049cde8a0986c50e977aab
Marcos Vinícius Rodrigues Pires de Lima <mvrpl.me@gmail.com>
2025-02-22 21:16:16 -0300

    update structure repo

---
+++ bin/auto-pr.ps1
@@ -0,0 +1,8 @@
+param(
+    [String]$upstream = "mvrpl/windows-apps:main"
+)
+
+if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
+$autopr = "$env:SCOOP_HOME/bin/auto-pr.ps1"
+$dir = "$PSScriptRoot/../bucket" # checks the parent dir
+& $autopr -Dir $dir -Upstream $Upstream @Args
